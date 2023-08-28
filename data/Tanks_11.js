8a0a95d23f83dc7a50bf0c2f8e29f53cf42407ba
GregTalisman <116227317+GregTalisman@users.noreply.github.com>
2023-08-27 23:17:26 -0600

    Add files via upload

--- data/Tanks_11.js
+++ data/Tanks_11.js
@@ -3,9 +3,9 @@ var json_Tanks_11 = {
 "name": "Tanks_11",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Tank": "Tank 3", "OF Elev": 6987.0, "Volume": "300,000" }, "geometry": { "type": "Point", "coordinates": [ -111.846941317208689, 41.195698808410526 ] } },
-{ "type": "Feature", "properties": { "Tank": "Tank 1", "OF Elev": 6782.5, "Volume": "300,000" }, "geometry": { "type": "Point", "coordinates": [ -111.857718658631924, 41.210053852966432 ] } },
-{ "type": "Feature", "properties": { "Tank": "Tank 2", "OF Elev": 8038.33, "Volume": "1,000,000" }, "geometry": { "type": "Point", "coordinates": [ -111.865219146784014, 41.196072712807776 ] } },
-{ "type": "Feature", "properties": { "Tank": "Tank 4", "OF Elev": 9227.0, "Volume": "50,000" }, "geometry": { "type": "Point", "coordinates": [ -111.872813693650045, 41.18653178304546 ] } }
+{ "type": "Feature", "properties": { "fid": "1", "Tank": "Tank 3", "OF Elev": 6987.0, "Volume": "300,000" }, "geometry": { "type": "Point", "coordinates": [ -111.846941317208689, 41.195698808410526 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Tank": "Tank 1", "OF Elev": 6782.5, "Volume": "300,000" }, "geometry": { "type": "Point", "coordinates": [ -111.857718658631924, 41.210053852966432 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Tank": "Tank 2", "OF Elev": 8038.33, "Volume": "1,000,000" }, "geometry": { "type": "Point", "coordinates": [ -111.865219146784014, 41.196072712807776 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Tank": "Tank 4", "OF Elev": 9227.0, "Volume": "50,000" }, "geometry": { "type": "Point", "coordinates": [ -111.872813693650045, 41.18653178304546 ] } }
 ]
 }
